41b1db02d2f3157b3774ba910dcbd8273a8d7454
Jiarui Han <zhongyangchuwu@qq.com>
2026-09-19 23:14:47 +0800

    feat: add plugin picker scope

--- src/main/picker/model.ts
+++ src/main/picker/model.ts
@@ -1,4 +1,4 @@
-export type PickerScope = 'all' | 'collection' | 'tabs' | 'notes' | 'tags' | 'commands';
+export type PickerScope =\n  | 'all'\n  | 'collection'\n  | 'tabs'\n  | 'notes'\n  | 'tags'\n  | 'plugins'\n  | 'commands';
 
 export interface PickerItem {
   id: string | number;
