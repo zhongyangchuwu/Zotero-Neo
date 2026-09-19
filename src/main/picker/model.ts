c33ff1a60c9800adbf651e049d652ca329999f97
Jiarui Han <zhongyangchuwu@qq.com>
2026-09-19 23:20:15 +0800

    style: format plugin picker scope

--- src/main/picker/model.ts
+++ src/main/picker/model.ts
@@ -1,11 +1,4 @@
-export type PickerScope =
-  | 'all'
-  | 'collection'
-  | 'tabs'
-  | 'notes'
-  | 'tags'
-  | 'plugins'
-  | 'commands';
+export type PickerScope = 'all' | 'collection' | 'tabs' | 'notes' | 'tags' | 'plugins' | 'commands';
 
 export interface PickerItem {
   id: string | number;
