990e781cc028a98302ba4cdd70cd5210bd3fa913
Jiarui Han <zhongyangchuwu@qq.com>
2026-09-20 01:02:08 +0800

    test: extend plugin fixtures with description metadata

--- tests/unit/plugin-manager.test.ts
+++ tests/unit/plugin-manager.test.ts
@@ -11,6 +11,7 @@ const plugins: readonly InstalledPlugin[] = [
     enabled: true,
     canEnable: true,
     canDisable: true,
+    description: '',
   },
   {
     id: 'zotero-actions-tags@windingwind.com',
@@ -19,6 +20,7 @@ const plugins: readonly InstalledPlugin[] = [
     enabled: false,
     canEnable: true,
     canDisable: true,
+    description: '',
   },
   {
     id: 'translate-for-zotero@example.org',
@@ -27,6 +29,7 @@ const plugins: readonly InstalledPlugin[] = [
     enabled: true,
     canEnable: true,
     canDisable: true,
+    description: '',
   },
 ];
 
