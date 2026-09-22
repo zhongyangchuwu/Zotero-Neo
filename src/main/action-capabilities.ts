a9166c54f90aec8fde0a32ea5b0a8e38dfc47889
Jiarui Han <zhongyangchuwu@qq.com>
2026-09-22 20:58:30 +0800

    feat: delegate return context from Reader

--- src/main/action-capabilities.ts
+++ src/main/action-capabilities.ts
@@ -81,6 +81,7 @@ export const READER_DELEGABLE_MAIN_ACTIONS = Object.freeze([
   'findCollectionItems',
   'findNotes',
   'managePlugins',
+  'mainReturnContext',
   'switchTab',
   'mainYankCitekey',
   'closeCurrentTab',
